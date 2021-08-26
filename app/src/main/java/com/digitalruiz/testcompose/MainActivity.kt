edffee126976031c2ffb2ba6e2c6ad4a7841e9fe
Reyes Ruiz <reyes_ruiz@digitalruiz.com>
2021-08-26 10:52:06 -0700

    Lesson 2, using modifiers to add columns, rows, images, text, size, and spacing.

--- app/src/main/java/com/digitalruiz/testcompose/MainActivity.kt
+++ app/src/main/java/com/digitalruiz/testcompose/MainActivity.kt
@@ -3,11 +3,18 @@ package com.digitalruiz.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.digitalruiz.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,15 +22,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             setContent {
-                MessageCard("Android")
+                MessageCard(Message("Android", "Jetpack COmpose"))
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
+
 @Composable
-fun MessageCard(name: String){
-    Text(text ="Hello $name!" )
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.l60hf),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 
@@ -31,6 +55,7 @@ fun MessageCard(name: String){
 @Composable
 fun PreviewMessageCard() {
     TestComposeTheme {
-        MessageCard("Android")
+        MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
     }
 }
